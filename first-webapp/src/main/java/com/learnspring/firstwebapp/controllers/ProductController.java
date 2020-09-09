d7ab7956ac1032ee91c1c67a3b75a460d3311308
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-09 17:26:27 +0300

    can get product page

---
+++ first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.learnspring.firstwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.learnspring.firstwebapp.entities.Product;
+import com.learnspring.firstwebapp.service.ProductService;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+  
+  private final ProductService service;
+   
+  public ProductController(ProductService service) {
+    this.service = service;
+  }
+
+  @GetMapping(params = {"id"})
+  public String product(Model model, @RequestParam(name = "id") Long id) {
+    Product product = service.getOne(id);    
+    model.addAttribute("product", product);   
+    return "product";
+    
+  }
+}
