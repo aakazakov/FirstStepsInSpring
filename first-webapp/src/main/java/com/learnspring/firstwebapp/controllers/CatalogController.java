1895e969b057f3ffd1c6aa15ab36b8931f019256
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-09 16:33:17 +0300

    can get catalog page

---
+++ first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/CatalogController.java
@@ -0,0 +1,30 @@
+package com.learnspring.firstwebapp.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.learnspring.firstwebapp.entities.Product;
+import com.learnspring.firstwebapp.service.ProductService;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+  
+  private final ProductService service;
+  
+  public CatalogController(ProductService service) {
+    this.service = service;
+  }
+  
+  @GetMapping
+  public String catalog(Model model) {
+    List<Product> products = service.getAll();
+    model.addAttribute("products", products);
+    return "catalog";
+  }
+  
+}
