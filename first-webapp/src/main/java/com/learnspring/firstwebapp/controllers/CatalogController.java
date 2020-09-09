6585194e30e6a524658b37aba1fb3c1a97d8bbb7
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-09 19:05:29 +0300

    trying to add a form

--- first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/CatalogController.java
+++ first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/CatalogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.learnspring.firstwebapp.entities.Product;
@@ -27,4 +28,9 @@ public class CatalogController {
     return "catalog";
   }
   
+  @PostMapping
+  public String addNewProduct(Product product) {
+    service.save(product);
+    return "redirect:/catalog/";
+  }
 }
