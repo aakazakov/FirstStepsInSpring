648e38788cb4c0c89bd92e4acf58688295a23b36
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-13 18:21:25 +0300

    refactorro)

--- hibernation/src/main/java/com/learnspring/hibernation/Administrator.java
+++ hibernation/src/main/java/com/learnspring/hibernation/Administrator.java
@@ -2,22 +2,33 @@ package com.learnspring.hibernation;
 
 import javax.persistence.EntityManager;
 
+import org.hibernate.cfg.Configuration;
+
 import com.learnspring.hibernation.domain.Domain;
 
 public class Administrator {
   
   private EntityManager em;
   
-  public Administrator(EntityManager em) {
-    this.em = em;
+  public Administrator() {
+    init();
+  }
+  
+  private void init() {
+    em = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .buildSessionFactory()
+        .createEntityManager();
   }
   
   public void createEntity(Domain entity) {
-    System.out.println("start creating");
     em.getTransaction().begin();
     em.persist(entity);
     em.getTransaction().commit();
-    System.out.println("creation completed successfully");
+  }
+  
+  public void close() {
+    em.close();
   }
   
 }
--- hibernation/src/main/java/com/learnspring/hibernation/App.java
+++ hibernation/src/main/java/com/learnspring/hibernation/App.java
@@ -1,30 +1,19 @@
 package com.learnspring.hibernation;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
-import org.hibernate.cfg.Configuration;
-
 import com.learnspring.hibernation.domain.Buyer;
 
 public class App {
   
   public static void main(String[] args) {
-
-    EntityManagerFactory entityFactory = new Configuration()
-        .configure("hibernate.cfg.xml")
-        .buildSessionFactory();
     
-    EntityManager em = entityFactory.createEntityManager();
-    
-    Administrator admin = new Administrator(em);
+    Administrator admin = new Administrator();
     
     Buyer buyer = new Buyer();
     buyer.setName("John");
     
     admin.createEntity(buyer);
     
-    em.close();
+    admin.close();
     
   }
   
