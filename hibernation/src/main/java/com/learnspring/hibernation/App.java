f07d8152904fb55b64f135556df535af5d65bf88
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-13 17:00:38 +0300

    the first steps have been taken )

--- hibernation/src/main/java/com/learnspring/hibernation/App.java
+++ hibernation/src/main/java/com/learnspring/hibernation/App.java
@@ -1,13 +1,31 @@
-package com.learspring.hibernation;
+package com.learnspring.hibernation;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.cfg.Configuration;
+
+import com.learnspring.hibernation.domain.Buyer;
+
+public class App {
+  
+  public static void main(String[] args) {
+
+    EntityManagerFactory entityFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .buildSessionFactory();
+    
+    EntityManager em = entityFactory.createEntityManager();
+    
+    Administrator admin = new Administrator(em);
+    
+    Buyer buyer = new Buyer();
+    buyer.setName("John");
+    
+    admin.createEntity(buyer);
+    
+    em.close();
+    
+  }
+  
 }
