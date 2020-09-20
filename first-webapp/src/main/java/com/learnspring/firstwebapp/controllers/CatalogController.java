3e0491b5072c84d692ffb6384133362261319017
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-20 17:10:20 +0300

    deleted unnecessary line

--- first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/CatalogController.java
+++ first-webapp/src/main/java/com/learnspring/firstwebapp/controllers/CatalogController.java
@@ -1,7 +1,6 @@
 package com.learnspring.firstwebapp.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
