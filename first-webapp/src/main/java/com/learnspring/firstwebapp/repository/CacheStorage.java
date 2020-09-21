dbc32c84480e4cf8f3e9cfe998a929c17b5a23a5
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-21 18:28:32 +0300

    add cache repo

---
+++ first-webapp/src/main/java/com/learnspring/firstwebapp/repository/CacheStorage.java
@@ -0,0 +1,48 @@
+package com.learnspring.firstwebapp.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.learnspring.firstwebapp.domain.Product;
+
+@Repository
+public class CacheStorage {
+
+  private List<Product> cache;
+  
+  public void setCache(List<Product> cache) {
+    this.cache = cache;
+  }
+  
+  public List<Product> getCache() {
+    return cache;
+  }
+  
+  public void cleanCache() {
+    if (cacheSize() > 0) cache.clear();
+  }
+  
+  public Product add(Product p) {
+    cache.add(p);
+    return cache.get(cacheSize() - 1);
+  }
+  
+  public void removeById(Long id) {
+    for (Product p : cache) {
+      if (p.getId() == id) {
+        cache.remove(p);
+        break;
+      }
+    }
+  }
+  
+  public List<Product> getSomeOfTheCache(int start, int end) {
+    return cache.subList(start, end);
+  } 
+  
+  public int cacheSize() {
+    return cache == null ? 0 : cache.size();
+  }
+  
+}
