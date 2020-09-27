7f6fb5f388e12688a828d0245871247d26b227cb
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-27 10:01:31 +0300

    preparation

---
+++ first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/FirstSimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.firstsimplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSimpleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSimpleRestApiApplication.class, args);
+	}
+
+}
---
+++ first-simple-rest-api/src/test/java/com/learnspring/firstsimplerestapi/FirstSimpleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.firstsimplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSimpleRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
