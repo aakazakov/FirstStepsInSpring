00b461f829b0e804f0c9d1a6cc9c86057ed59c3b
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-28 14:11:00 +0300

    added dto obj

---
+++ first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/dto/ProductDto.java
@@ -0,0 +1,23 @@
+package com.learnspring.firstsimplerestapi.dto;
+
+public class ProductDto {
+  
+  private Integer id;
+  private String title;
+  private Double cost;
+  
+  public Integer getId() {
+    return id;
+  }
+  
+  public void setId(Integer id) { this.id = id; }
+  
+  public String getTitle() { return title; }
+  
+  public void setTitle(String title) { this.title = title; }
+  
+  public Double getCost() { return cost; }
+  
+  public void setCost(Double cost) { this.cost = cost; }
+   
+}
