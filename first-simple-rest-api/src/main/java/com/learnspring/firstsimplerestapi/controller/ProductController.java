b141de1178f9121ef33bd9bd6d2055baf431499e
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-28 10:16:08 +0300

    get by id

--- first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/controller/ProductController.java
+++ first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.learnspring.firstsimplerestapi.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,13 @@ public class ProductController {
   }
   
   @GetMapping
-  public List<Product> getAllProducts() {
+  public List<Product> getAll() {
     return service.getAll();
+  }
+  
+  @GetMapping("/{id}")
+  public Product getOne(@PathVariable(name = "id") Integer id) {
+    return service.getById(id);
   }
   
 }
