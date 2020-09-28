9372e9475b5fe69897c80b6a82dde4e565ed6b37
andrey_kazakov <aakazakovv@yandex.ru>
2020-09-28 13:33:06 +0300

    can get by cost and by expired date

--- first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/controller/ProductController.java
+++ first-simple-rest-api/src/main/java/com/learnspring/firstsimplerestapi/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnspring.firstsimplerestapi.domain.Product;
@@ -22,12 +23,24 @@ public class ProductController {
   
   @GetMapping
   public List<Product> getAll() {
-    return service.getAll();
+    return service.findAll();
   }
   
   @GetMapping("/{id}")
   public Product getOne(@PathVariable(name = "id") Integer id) {
-    return service.getById(id);
+    return service.findById(id);
+  }
+  
+  @GetMapping(params = "exp")
+  public List<Product> getByExpiredDate(@RequestParam(name = "exp") String expiredDate) {
+    return service.findByExpiredDateLessThanEqual(expiredDate);
+  }
+  
+  @GetMapping(params = {"min", "max"})
+  public List<Product> getByCost(
+      @RequestParam(name = "min") Double min,
+      @RequestParam(name = "max") Double max) {
+    return service.findByCostBetween(min, max);
   }
   
 }
