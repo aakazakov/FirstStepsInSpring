e19f28fbbe27b40b6b8f8ab426adfd24c674386d
andrey_kazakov <aakazakovv@yandex.ru>
2020-10-02 11:28:21 +0300

    preparation

---
+++ first-simple-secure-app/src/main/java/com/learnspring/firstsimplesecureapp/FirstSimpleSecureAppApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.firstsimplesecureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSimpleSecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSimpleSecureAppApplication.class, args);
+	}
+
+}
---
+++ first-simple-secure-app/src/test/java/com/learnspring/firstsimplesecureapp/FirstSimpleSecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.firstsimplesecureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSimpleSecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
